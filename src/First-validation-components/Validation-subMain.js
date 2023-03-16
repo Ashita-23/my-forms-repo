18dfa8933b90e9b51a9f19fa8295fc671bc0ea9d
Akash <akashbytes@gmail.com>
2023-03-16 22:36:21 +0530

    code edited in form file

--- src/First-validation-components/Validation-subMain.js
+++ src/First-validation-components/Validation-subMain.js
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SubMainForm = () => {
+  const [formData, setNewFormData] = useState({
+    userFName: " ",
+    userLName: " ",
+    userEmailId: " ",
+    userPassword: " ",
+  });
 
+  const OnFName = (event) => {
+    event.preventDefault();
+    // console.log(event.target.value)
+    const name = event.target.name;
+    const value = event.target.value;
+   setNewFormData((preValue) =>{
+    if(name === "userFName"){
+     return{
+      userFName: value,
+      userLName: preValue.userLName,
+      userEmailId: preValue.userEmailId,
+      userPassword:preValue.userPassword,
+     }
+    } else if(name === "userLName"){
+      return{
+       userFName: preValue.userFName,
+       userLName: value,
+       userEmailId: preValue.userEmailId,
+       userPassword:preValue.userPassword,
+      }
+     } else if(name === "userEmailId"){
+      return{
+       userFName: preValue.userFName,
+       userLName: preValue.userLName,
+       userEmailId: value,
+       userPassword:preValue.userPassword,
+      }
+     }else if(name === "userPassword"){
+      return{
+       userFName: preValue.userFName,
+       userLName: preValue.userLName,
+       userEmailId: preValue.userEmailId,
+       userPassword:value,
+      }
+     }
+   })
+  };
 
-
-
-
+  const OnSubmit = ( event) =>{
+    event.preventDefault()
+    
+  }
 
   return (
     <>
       <div className="main-outer">
         <div className="form-box">
-          <form action="" autoFill={"off"}>
+          <form action="" autoComplete="off" onSubmit={OnSubmit}>
             <div>
               <label htmlFor="userName">First Name</label>
               <input
                 type={"text"}
                 name={"userFName"}
                 placeholder={"enter first name  "}
-                value={""}
+                value={formData.userFName}
+                onChange={OnFName}
+                autoComplete="off"
               />
             </div>
             <div>
@@ -27,29 +73,37 @@ const SubMainForm = () => {
                 type={"text"}
                 name={"userLName"}
                 placeholder={"enter last name "}
-                value={""}
+                value={formData.userLName}
+                onChange={OnFName}
+                autoComplete="off"
               />
             </div>
             <div>
               <label htmlFor="userName">Email id </label>
               <input
                 type={"email"}
-                name={"userEmail"}
+                name={"userEmailId"}
                 placeholder={"enter emailxx.com "}
-                value={""}
+                value={formData.userEmailId}
+                onChange={OnFName}
+                autoComplete="off"
               />
             </div>
             <div>
               <label htmlFor="userName">password</label>
               <input
                 type={"password"}
-                name={"password"}
+                name={"userPassword"}
                 placeholder={"******"}
-                value={""}
+                value={formData.userPassword}
+                onChange={OnFName}
+                autoComplete={"off"}
               />
             </div>
             <div>
-              <button type={"submit"} className="form-btn">Submit</button>
+              <button type={"submit"} className="form-btn">
+                Submit
+              </button>
             </div>
           </form>
         </div>
